e34660a83be456072095d8a9dc7b2b87bd4e7b7e
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-08 11:38:09 +0530

    New URLs and function

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,26 +4,20 @@ from django.shortcuts import render
 def dashboard_page(request):
     return render(request,'dashboard.html')
 
-
 def sos_page(request):
     return render(request,'sos.html')
-
 
 def contacts_page(request):
     return render(request,'contacts.html')
 
-
 def journey_page(request):
     return render(request,'start_journey.html')
-
 
 def places_page(request):
     return render(request,'safe_places.html')
 
-
 def reports_page(request):
     return render(request,'report.html')
-
 
 def history_page(request):
     return render(request,'journey_status.html')
