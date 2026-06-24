477e70bf1230bbd0cb5e042b26b272bf4a8fa992
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-24 13:26:36 +0530

    Authentication

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import UserProfile, EmergencyContact
 
 admin.site.register(UserProfile)
-admin.site.register(EmergencyContact)
+# admin.site.register(EmergencyContact)
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(EmergencyContact)
-admin.site.register(Journey)
-admin.site.register(UnsafeReport)
+# admin.site.register(EmergencyContact)
+# admin.site.register(Journey)
+# admin.site.register(UnsafeReport)
 admin.site.register(SOSAlert)
