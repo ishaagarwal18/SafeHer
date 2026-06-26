496cb5f2477c215d33de96b0d589d495eec13174
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-26 13:51:58 +0530

    Changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class SOSAlert(models.Model):
-    alert_time = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20,default="Triggered")
-    location = models.CharField(max_length=255,blank=True)
-    def __str__(self):
-        return self.status
+    alert_time=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50)
+    latitude = models.CharField(max_length=50, blank=True, null=True)
+    longitude = models.CharField(max_length=50, blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,42 @@
+# from django.shortcuts import render
+# from .models import SOSAlert
+
+
+# def dashboard_page(request):
+#     return render(request,'dashboard.html')
+
+# def sos_page(request):
+
+#     if request.method == "POST":
+
+#         SOSAlert.objects.create(
+#             location="Ahmedabad"
+#         )
+
+#     alerts = SOSAlert.objects.all()
+
+#     return render(
+#         request,
+#         "sos.html",
+#         {"alerts": alerts}
+#     )
+
+# def contacts_page(request):
+#     return render(request,'contacts.html')
+
+# def journey_page(request):
+#     return render(request,'start_journey.html')
+
+# def places_page(request):
+#     return render(request,'safe_places.html')
+
+# def reports_page(request):
+#     return render(request,'report.html')
+
+# def history_page(request):
+#     return render(request,'journey_status.html')
+
+
 from django.shortcuts import render
 from .models import SOSAlert
 
@@ -6,20 +45,24 @@ def dashboard_page(request):
     return render(request,'dashboard.html')
 
 def sos_page(request):
+    if request.method=="POST":
 
-    if request.method == "POST":
+        location=request.POST.get("location")
+        latitude=request.POST.get("latitude")
+        longitude=request.POST.get("longitude")
 
         SOSAlert.objects.create(
-            location="Ahmedabad"
+
+            status="Sent",
+            location=location,
+            latitude=latitude,
+            longitude=longitude
+
         )
 
-    alerts = SOSAlert.objects.all()
+    alerts=SOSAlert.objects.all().order_by("-id")
 
-    return render(
-        request,
-        "sos.html",
-        {"alerts": alerts}
-    )
+    return render(request,"sos.html",{"alerts":alerts})
 
 def contacts_page(request):
     return render(request,'contacts.html')
