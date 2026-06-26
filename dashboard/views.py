8b1ec4d7c52606d152f6dc55da13c30ca59608cc
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-26 14:30:18 +0530

    Button

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,48 +1,15 @@
-# from django.shortcuts import render
-# from .models import SOSAlert
-
-
-# def dashboard_page(request):
-#     return render(request,'dashboard.html')
-
-# def sos_page(request):
-
-#     if request.method == "POST":
-
-#         SOSAlert.objects.create(
-#             location="Ahmedabad"
-#         )
-
-#     alerts = SOSAlert.objects.all()
-
-#     return render(
-#         request,
-#         "sos.html",
-#         {"alerts": alerts}
-#     )
-
-# def contacts_page(request):
-#     return render(request,'contacts.html')
-
-# def journey_page(request):
-#     return render(request,'start_journey.html')
-
-# def places_page(request):
-#     return render(request,'safe_places.html')
-
-# def reports_page(request):
-#     return render(request,'report.html')
-
-# def history_page(request):
-#     return render(request,'journey_status.html')
-
-
-from django.shortcuts import render
+from authentication.models import EmergencyContact
+from journey.models import Journey
+from reports.models import UnsafeReport
 from .models import SOSAlert
-
+from django.shortcuts import render
 
 def dashboard_page(request):
-    return render(request,'dashboard.html')
+    context={"journey_count":Journey.objects.count(),
+            "contact_count":EmergencyContact.objects.count(),
+            "sos_count":SOSAlert.objects.count(),
+            "report_count":UnsafeReport.objects.count(),}
+    return render(request,"dashboard.html",context)
 
 def sos_page(request):
     if request.method=="POST":
