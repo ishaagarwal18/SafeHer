850d4d9d404a8b19e42b6aca5235a8f7ec1bbc28
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-07-27 16:48:37 +0530

    Changes

--- Backend/authentication/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, EmergencyContact
-
-admin.site.register(UserProfile)
-# admin.site.register(EmergencyContact)
--- Backend/dashboard/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# admin.site.register(EmergencyContact)
-# admin.site.register(Journey)
-# admin.site.register(UnsafeReport)
-admin.site.register(SOSAlert)
