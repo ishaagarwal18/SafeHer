13a338886a294a9f68262142359243dba8bd456b
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-08-01 21:45:22 +0530

    Changes

--- Backend/journey/views.py
+++ Backend/journey/views.py
@@ -2,28 +2,13 @@ from django.shortcuts import render
 from .models import Journey
 
 def journey_page(request):
-<<<<<<< HEAD
     if request.method == "POST":
-=======
-
-    if request.method == "POST":
-
->>>>>>> 638ded2b3cfbe47f6a9e7fe38770d4a478977c07
         Journey.objects.create(
             source=request.POST["source"],
             destination=request.POST["destination"],
             transport_mode=request.POST["transport"]
         )
-<<<<<<< HEAD
-    journeys = Journey.objects.all()
-    return render(request,"start_journey.html",{"journeys": journeys})
-=======
 
     journeys = Journey.objects.all()
+    return render(request, "start_journey.html", {"journeys": journeys})
 
-    return render(
-        request,
-        "start_journey.html",
-        {"journeys": journeys}
-    )
->>>>>>> 638ded2b3cfbe47f6a9e7fe38770d4a478977c07
