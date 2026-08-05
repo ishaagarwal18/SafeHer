1ff19f43889212263ce86f019eef874d9b6c83a2
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-08-05 19:00:24 +0530

    Support creating multiple superusers automatically via SUPERUSERS env var

--- Backend/build.sh
+++ Backend/build.sh
@@ -7,8 +7,5 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
-# Automatically create admin superuser if environment variables are set
-if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    echo "Creating superuser..."
-    python manage.py createsuperuser --noinput || true
-fi
+# Automatically create admin superusers if environment variables are set
+python manage.py create_superusers || true
